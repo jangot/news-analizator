04fa6692f047840cfc7212ae4e33f3be7ce088f6
ppulin <pavel.pulin@smile2impress.com>
2024-09-19 14:18:58 +0300

    load new by days

--- routes/news.js
+++ routes/news.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const axios = require('axios');
+const { XMLParser } = require('fast-xml-parser');
 
 const config = require('../configuration');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const { data } = axios.get(`${config.riaUrl}/20240918/index.xml`)
-  res.send('respond with a resource');
+router.get('/:date', async (req, res, next) => {
+  // const date = req.params.date;20240918
+  const { data } = await axios.get(`${config.riaUrl}/${req.params.date}/index.xml`);
+
+  const parser = new XMLParser();
+  const result = parser.parse(data);
+
+  res.render('news', { title: 'RIA', items: result.rss.channel.item });
 });
 
 module.exports = router;
