02397cf7fe996af6cecf3eba330cc6088ddd1445
ppulin <pavel.pulin@smile2impress.com>
2024-10-07 00:54:42 +0300

    save data

--- bin/save-data.js
+++ bin/save-data.js
@@ -1,4 +1,4 @@
-const { fs } = require('fs/promises');
+const fs = require('fs/promises');
 
 const config = require('../configuration');
 const { runScript } = require('../services/run-script');
@@ -6,12 +6,24 @@ const { sequelize } = require('../models');
 const {getQuery} = require('../services/analyzer');
 
 const FROM = '2024-09-01';
-const TO = '2024-09-10';
+const TO = '2024-10-01';
+
+function fileExists(path) {
+    return fs.stat(path).then(() => true, () => false)
+}
 
 runScript(async () => {
     const query = getQuery(FROM, TO, 0);
     const [ list] = await sequelize.query(query);
 
-    
+    for (let i = 0; i < list.length; i++) {
+        const it = list[i];
+        const filePame = `${config.files.destinationFolder}/${it.day}.json`;
+        const exists = await fileExists(filePame);
+        if (exists) {
+            await fs.unlink(filePame)
+        }
+        await fs.writeFile(filePame, JSON.stringify({ keywords: it.keywords_count }, null, 4), 'utf8');
+    }
 
 }, 'Save files');
