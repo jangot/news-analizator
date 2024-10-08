5774b1711a70211c225f3298a2897434452021e7
ppulin <pavel.pulin@smile2impress.com>
2024-10-08 19:11:12 +0300

    date as params

--- bin/scan.js
+++ bin/scan.js
@@ -1,34 +1,61 @@
+const axios = require('axios');
+const moment = require('moment');
+const commandLineArgs = require('command-line-args');
+
 const config = require('../configuration');
 const { loadDayPortion, loadPosts } = require('../services/post-loader');
 const { runScript } = require('../services/run-script');
 
-// const START_DAY = 1;
-// const LAST_DAY = 30;
-const START_DAY = 5;
-const LAST_DAY = 7;
-const otherDate = '202410';
+const todayMorning = moment();
+const {
+    year = todayMorning.format('YYYY'),
+    month = todayMorning.format('MM'),
+    startDay = todayMorning.date(),
+    endDay = startDay,
+    waiting = 100,
+} = commandLineArgs([
+    { name: 'year', alias: 'y' },
+    { name: 'month', alias: 'm' },
+    { name: 'startDay', alias: 's' },
+    { name: 'endDay', alias: 'e' },
+    { name: 'waiting', alias: 'w' },
+]);
+
+function wait(time) {
+    console.log('waiting', time);
+    return new Promise((resolve) => {
+        setTimeout(resolve, Number(time));
+    });
+}
 
 async function loadDay(date) {
     let url = `${config.riaUrl}/services/${date}/more.html?id=0&date=${date}T235959`;
     while (url) {
         console.log('NEXT PORTION ->', url);
-        let { links, nextUrl } = await loadDayPortion(url);
-        url = nextUrl ? `${config.riaUrl}${nextUrl}` : null;
+        try {
+            let { links, nextUrl } = await loadDayPortion(url);
+            url = nextUrl ? `${config.riaUrl}${nextUrl}` : null;
 
-        await loadPosts(links);
+            await loadPosts(links);
+            await wait(waiting);
+        } catch (err) {
+            console.log('GETTING -->', url);
+            throw axios.isAxiosError(err) ? err.toJSON() : err;
+        }
     }
 }
 
 runScript(async () => {
-    let day = START_DAY
+    const baseDate = `${year}${month}`;
+    let day = Number(startDay)
 
-    while (day <= LAST_DAY) {
-        const date = otherDate + String(day).padStart(2, '0');
+    while (day <= endDay) {
+        const date = baseDate + String(day).padStart(2, '0');
         try {
             await loadDay(date);
         } catch (error) {
             console.error(error);
-            console.log('Scan stopped', date)
+            console.log('Scan stopped -->', date)
             break
         }
         day++;
